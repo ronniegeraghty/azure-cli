c084a52925aa0ced70e97de4a773d5512121a22e
Hang Lei <2227874+bebound@users.noreply.github.com>
2022-06-15 11:55:23 +0800

    Init
    Minor fix
    
    Change artifact name
    
    Fix cannot open linker script file error on Fedora36
    
    Minor fix
    
    Minor fix
    
    Change bug url
    
    Minor fix
    
    Minor fix
    
    Minor fix
    
    Update package name in test
    
    Debug
    
    Debug2
    
    Minor fix
    
    Minor fix
    
    Minor fix
    
    Fix python lib path on Fedora36
    
    Fix Fedora36 PYTHONPATH
    
    Minor fix
    
    Minor fix

--- scripts/release/rpm/test_rpm_in_docker.sh
+++ scripts/release/rpm/test_rpm_in_docker.sh
@@ -7,7 +7,7 @@ export USERNAME=azureuser
 
 yum --nogpgcheck localinstall /mnt/rpm/$RPM_NAME -y
 
-yum install git gcc python3-devel -y
+yum install git gcc python3-devel findutils -y
 
 ln -s -f /usr/bin/python3 /usr/bin/python
 ln -s -f /usr/bin/pip3 /usr/bin/pip
@@ -17,6 +17,15 @@ time az --version
 cd /azure-cli/
 pip3 install wheel
 ./scripts/ci/build.sh
+
+# From Fedora36, when using `pip install --prefix`with root, the package is installed into `{prefix}/local/lib`.
+# Move installed az modules to make sure tests can be installed correctly later
+# Ref https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
+if python3 -c "import sysconfig; print('local' in sysconfig.get_path('platlib'))" | grep True; then
+  mkdir -p /usr/lib64/az/local/
+  mv /usr/lib64/az/lib/ /usr/lib64/az/local/
+fi
+
 pip3 install pytest --prefix /usr/lib64/az
 pip3 install pytest-xdist --prefix /usr/lib64/az
 
--- scripts/release/rpm/test_rpm_package.py
+++ scripts/release/rpm/test_rpm_package.py
@@ -7,10 +7,20 @@ import os
 import sys
 import subprocess
 
-root_dir = '/usr/lib64/az/lib/python3.6/site-packages/azure/cli/command_modules'
+
+# From Fedora 36, use `/usr/lib64/az/local/lib/` as PYTHONPATH because installation scheme has changed.
+# `test_rpm_in_docker.sh` has moved az module to here.
+# Ref https://docs.fedoraproject.org/en-US/fedora/latest/release-notes/developers/Development_Python/
+if os.path.exists('/usr/lib64/az/local/'):
+    python_version = os.listdir('/usr/lib64/az/local/lib/')[0]
+    pytest_base_cmd = f'PYTHONPATH=/usr/lib64/az/local/lib/{python_version}/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
+    root_dir = f'/usr/lib64/az/local/lib/{python_version}/site-packages/azure/cli/command_modules'
+else:
+    python_version = os.listdir('/usr/lib64/az/lib/')[0]
+    pytest_base_cmd = f'PYTHONPATH=/usr/lib64/az/lib/{python_version}/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
+    root_dir = f'/usr/lib64/az/lib/{python_version}/site-packages/azure/cli/command_modules'
 mod_list = [mod for mod in sorted(os.listdir(root_dir)) if os.path.isdir(os.path.join(root_dir, mod)) and mod != '__pycache__']
 
-pytest_base_cmd = 'PYTHONPATH=/usr/lib64/az/lib/python3.6/site-packages python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN'
 pytest_parallel_cmd = '{} -n auto'.format(pytest_base_cmd)
 serial_test_modules = ['botservice', 'network', 'cloud', 'appservice']
 
